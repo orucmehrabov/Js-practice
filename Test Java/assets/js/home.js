efc957209334b09d9ddc47594ec556ac7d7b78e9
Oruj Mehrabov <102570437+orucmehrabov@users.noreply.github.com>
2024-01-28 04:42:17 +0400

    first commit

---
+++ Test Java/assets/js/home.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// console.log("Hello world");
+
+// var surname = "Mehrabov";
+// console.log(surname);
+// // let address = "test";
+// let email = "orucmehrabov@gmail.com";
+// console.log(email);
+// const phoneNumber = "+994704221997";
+// console.log(phoneNumber);
+
+// let surname;
+// console.log(surname);
+
+// let isMarried = true;
+// isMarried = 100;
+// console.log(isMarried);
+
+// let price = 100;
+// price += 50;
+// console.log(price);
+
+// const stu = {
+//     name: "Oruc",
+//     surname: "Mehrabov",
+//     age: 26
+// }
+
+// const stu2 = {
+//     name: "test",
+//     surname: "test7",
+//     age: 20
+// }
+
+// stu = stu2;
+
+// console.log(stu.name + " " + stu.surname + " " + stu.age);
+
+// let name = "ZZZ";
+// let surname = "XXX";
+// let age = "23";
+
+// let fullName = name + " " + surname + " " + age;
+
+// let fullName1 = "$(name) $(surname) $(age)";
+// console.log(fullName);
+
+// let ages = [24, 53, 73 , 74 , 73, "salam"];
+// console.log(ases[ages.length - 1]);
+
+// let num1 = 100;
+// let num2 = 200;
+
+// let result = num1 + num2;
+// console.log(result);
+
+// let num1 = "55";
+// let num2 = 55;
+
+// let result = num1 + num2;
+// console.log(result);
+
+// let num = 50
+// num*=2;
+// console.log(num);
+
+// let num1 = "46"
+// let num2 = 46
+
+// console.log (num1 == num2);
+
+let number = 100;
+if (number>90 && number<120) {
+    console.log("yes");
+}else {console.log ("no")}
+
+let result = number == 100 ? "yes" : "no";
+console.log(result);
